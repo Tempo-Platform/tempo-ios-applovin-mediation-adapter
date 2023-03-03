6f7a517cea2bacc96f72239108249e282638040b
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-03-03 17:41:57 +1100

    (feat) external UTC timestamp, podspec -> 0.2.7

--- TempoAdapter/ALTempoMediationAdapter.swift
+++ TempoAdapter/ALTempoMediationAdapter.swift
@@ -14,11 +14,11 @@ public class ALTempoMediationAdapter  : ALMediationAdapter, MAInterstitialAdapte
     var rewardedDelegate: MARewardedAdapterDelegate? = nil
 
     public override var sdkVersion : String {
-        return "0.2.6"
+        return "0.2.7"
     }
 
     public override var adapterVersion : String {
-        return "0.2.6"
+        return "0.2.7"
     }
     
     public override func initialize(with parameters: MAAdapterInitializationParameters, completionHandler: @escaping (MAAdapterInitializationStatus, String?) -> Void) {
--- tempo-applovin-mediation.podspec
+++ tempo-applovin-mediation.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |spec|
 
   spec.name         = "tempo-applovin-mediation"
-  spec.version      = "0.2.6"
+  spec.version      = "0.2.7"
   spec.summary      = "Tempo AppLovin iOS Mediation Adapter."
 
   spec.description  = <<-DESC
