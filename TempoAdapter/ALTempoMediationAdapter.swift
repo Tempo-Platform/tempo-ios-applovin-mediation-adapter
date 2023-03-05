034231bf53fd4b0ba881bd1bdd18edf6697a0538
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-03-06 09:08:21 +1100

    (fix) incorrect folder for new file, podspec -> 0.2.8

--- TempoAdapter/ALTempoMediationAdapter.swift
+++ TempoAdapter/ALTempoMediationAdapter.swift
@@ -14,11 +14,11 @@ public class ALTempoMediationAdapter  : ALMediationAdapter, MAInterstitialAdapte
     var rewardedDelegate: MARewardedAdapterDelegate? = nil
 
     public override var sdkVersion : String {
-        return "0.2.7"
+        return "0.2.8"
     }
 
     public override var adapterVersion : String {
-        return "0.2.7"
+        return "0.2.8"
     }
     
     public override func initialize(with parameters: MAAdapterInitializationParameters, completionHandler: @escaping (MAAdapterInitializationStatus, String?) -> Void) {
